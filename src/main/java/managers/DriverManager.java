2160364e87b752e2da9a942ae436ed4c175ddae2
Harika Kada <harikakada1024@outlook.com>
2025-09-19 23:14:40 +0530

    Updated DriverManager.java

--- src/main/java/managers/DriverManager.java
+++ src/main/java/managers/DriverManager.java
@@ -1,5 +1,59 @@
 package managers;
 
-public class DriverManager {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
+public class DriverManager
+{
+    private static WebDriver driver;
+
+    private DriverManager() {};
+
+    public static WebDriver getDriver() {
+
+        if (driver == null) {
+            String browser = ConfigManager.getBrowser("browser");
+            switch (browser) {
+
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--disable-notifications");
+                    chromeOptions.addArguments("--start-maximized");
+                    driver = new ChromeDriver(chromeOptions);
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    firefoxOptions.setCapability("unhandledPromptBehavior", "ignore");
+                    firefoxOptions.addArguments("--disable-notifications");
+                    firefoxOptions.addArguments("--start-maximized");
+                    driver = new FirefoxDriver(firefoxOptions);
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    EdgeOptions edgeOptions = new EdgeOptions();
+//                    edgeOptions.addArguments("--disable-notifications");
+                    edgeOptions.addArguments("--start-maximized");
+                    driver = new EdgeDriver(edgeOptions);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unsupported Browser: " + browser);
+            }
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
